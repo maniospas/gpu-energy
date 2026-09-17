29b49ce45843279076ffd0dc065483419bed4bbb
maniospas <maniospas@hotmail.com>
2026-09-17 14:47:24 +0300

    improved file names

---
+++ 2_profile.py
@@ -0,0 +1,19 @@
+# 2_profile.py
+import requests # pip install requests
+from energy.energy import Energy
+
+model_name = "llama3.2:latest"
+
+def ollama(prompt: str):
+    response = requests.post(
+        "http://localhost:11434/api/generate",
+        json={ "model": model_name, "prompt": prompt, "stream": False})
+    if response.status_code!=200: return 0 # skip this measurement
+    result = response.json()
+    return result["prompt_eval_count"]+result["eval_count"]
+
+energy = Energy()
+for prompt in [
+    "Who am I?",
+    "To be or not to be?"
+]: energy.record(model_name, lambda: ollama(prompt), repetitions=5, time_profile="idle")
